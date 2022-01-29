6a9ad90757ff02d8f88845d68ad78069fc493f83
Plehov Evgeny <plehov1992@yandex.ru>
2022-01-29 11:12:45 +0500

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         BmiService servise = new BmiService();
-        double bmi = servise.calculate(1.80, 75);
+        double bmi = servise.calculate(1.80, 75); //рост в метрах
 
         System.out.printf("Ваш индекс массы тела - " + bmi);
     }
